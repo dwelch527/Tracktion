7caab36dff4086bf6e70c3ec927c713b01d7b96a
Dylan Welch <dwelch527@gmail.com>
2017-07-22 17:20:07 -0500

    initial commit :celebration:

---
+++ Traktion/Traktion/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Traktion
+//
+//  Created by Dylan Welch on 7/22/17.
+//  Copyright © 2017 Dylan Welch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
